f4bc87cf7ab12c49ba4f192627f689040ea4747b
mobergmann <mobergmann@pm.me>
2022-11-25 20:51:47 +0100

    adding ssl support

--- main.py
+++ main.py
@@ -244,24 +244,18 @@ def send_santa_invitations(sender: Sender, password: str, santas: "dict[SecretSa
 
     try:
         # setup smtp for sending mails
-        with smtplib.SMTP(sender.address, sender.port) as server:
-            server.ehlo()
-
-            # secure connection using starttls
-            server.starttls(context=context)
-
-            server.ehlo()
-
+        with smtplib.SMTP_SSL(sender.address, sender.port) as server:
             # login to server
             server.login(sender.email, password)
 
             # send all mails to every santa
             for santa in santas:
+                print(f"Sending email to {santa.name}: {santa.email} ...")
                 msg = construct_message(santa, santas[santa], sender)
                 server.send_message(msg)
 
     except Exception as e:
-        raise Exception("Error while sending at least one email.")
+        raise Exception("Error while sending at least one email.", e)
 
 
 def main():
@@ -286,7 +280,7 @@ def main():
         password = getpass.getpass("Please enter the password for your email address: ")
         print()
 
-        print("Sending email notifications...")
+        print("Sending emails to the santas...")
         send_santa_invitations(sender, password, santas_assigned)
         print("Done\n")
 
